6bb635459933e07eeb372cb227070b33a9f6a9de
Kun Chen <quentin.chen@foxmail.com>
2017-02-18 12:15:58 -0800

    Code clean

--- flaskr.py
+++ flaskr.py
@@ -1,40 +1,43 @@
 #!/usr/bin/env python
 # encoding: utf-8
+"""
+Flaskr is a light weight blog system.
+"""
 
 # all imports
 import os
 import sqlite3
 
-from flask import (Flask, abort, flash, g, redirect, render_template, request,
+from flask import (Flask, flash, g, redirect, render_template, request,
                    session, url_for)
 
-app = Flask(__name__)
+__app__ = Flask(__name__)
 
 # Load default config and override config from an environment variable
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+__app__.config.update(dict(
+    DATABASE=os.path.join(__app__.root_path, 'flaskr.db'),
     DEBUG=True,
     SECRET_KEY='quentin',
     USERNAME='admin',
     PASSWORD='admin'
 ))
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+__app__.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
 def connect_db():
     """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
+    _rv = sqlite3.connect(__app__.config['DATABASE'])
+    _rv.row_factory = sqlite3.Row
+    return _rv
 
 
 def init_db():
     """Initiate database."""
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+    with __app__.app_context():
+        _db = get_db()
+        with __app__.open_resource('schema.sql', mode='r') as schema:
+            _db.cursor().executescript(schema.read())
+        _db.commit()
 
 
 def get_db():
@@ -45,38 +48,41 @@ def get_db():
     return g.sqlite_db
 
 
-@app.teardown_appcontext
-def close_db(error):
+@__app__.teardown_appcontext
+def close_db():
     """Close the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
 
-@app.route('/')
+@__app__.route('/')
 def show_entries():
-    db = get_db()
-    cur = db.execute('SELECT title, text FROM entries ORDER BY id DESC;')
+    """Get all the entries and render it with template."""
+    _db = get_db()
+    cur = _db.execute('SELECT title, text FROM entries ORDER BY id DESC;')
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
 
 
-@app.route('/add/', methods=['POST'])
+@__app__.route('/add/', methods=['POST'])
 def add_entry():
-    db = get_db()
-    db.execute('INSERT INTO entries (title, text) VALUES (?, ?)',
-               [request.form['title'], request.form['text']])
-    db.commit()
+    """Add new entry."""
+    _db = get_db()
+    _db.execute('INSERT INTO entries (title, text) VALUES (?, ?)',
+                [request.form['title'], request.form['text']])
+    _db.commit()
     flash('New entry has been successfully posted!')
     return redirect(url_for('show_entries'))
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@__app__.route('/login', methods=['GET', 'POST'])
 def login():
+    """Login with username and password."""
     error = None
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
+        if request.form['username'] != __app__.config['USERNAME']:
             error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
+        elif request.form['password'] != __app__.config['PASSWORD']:
             error = 'Invalid password'
         else:
             session['logged_in'] = True
@@ -85,12 +91,13 @@ def login():
     return render_template('login.html', error=error)
 
 
-@app.route('/logout')
+@__app__.route('/logout')
 def logout():
+    """Logout."""
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    __app__.run(debug=True)
